05017b88fd9fe6f23c423c0d28f253f237bb4a3c
Daniel Rosales <drosales9135@gmail.com>
2020-12-08 14:00:43 -0500

    Initial commit

---
+++ States_Capitals.java
@@ -0,0 +1,74 @@
+package state_Capitals;
+import java.util.HashMap; 
+
+public class States_Capitals {
+
+	public static void main(String[] args) {
+		HashMap<String, String> capitalCities = new HashMap<String, String>();
+			
+		
+		
+		capitalCities.put("Alabama"	,"Montgomery");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("California"	,"Sacramento");
+
+		capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Connecticut", "Hartford");
+	    capitalCities.put("Delaware", "Dover");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+	    
+	    capitalCities.put("Colorado","Denver");
+	    capitalCities.put("Alaska", "Juneau");
+	    capitalCities.put("Arizona", "Phoenix");
+	    capitalCities.put("Arkansas", "	Little Rock");
+	    capitalCities.put("Alabama"	,"Montgomery");
+		
+
+	}
+
+}
